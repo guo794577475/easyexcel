7f2cbec93eb7359c1b89ad69c3384b54262d2c7c
guoxifei <gxf820@163.com>
2020-12-31 14:30:39 +0800

    测试

--- src/test/java/com/example/demo/ExcelApplicationTests.java
+++ src/test/java/com/example/demo/ExcelApplicationTests.java
@@ -15,57 +15,15 @@ class ExcelApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("================");
-        List<String> strings = Arrays.asList("das", "few", "asd", "asdaw");
-        Collections.sort(strings, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.compareTo(o2);
-            }
-        });
-        MyLamda myLamda = s -> System.out.println(s + "yy");
-//        myLamda = new MyLambdaImpl();
-        myLamda.testLambda("testLambda");
-
-        lambda2();
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("runnable1");
-            }
-        };
-        runnable.run();
-        Runnable runnable1 = () -> {
-            System.out.println("run2");
-        };
-        runnable1.run();
-        Thread thread = new Thread(runnable);
-        Thread thread1 = new Thread(runnable1);
-        thread.start();
-        thread1.start();
-        TitileVO titileVO = null;
-        Optional<TitileVO> titileVO1 = Optional.ofNullable(titileVO);
-    }
-
-
-    private void lambda2() {
-        System.out.println("================");
-        List<String> strings = Arrays.asList("das", "few", "bfew", "asd", "asdaw");
-        Collections.sort(strings,(a, b)-> a.compareTo(b));
-        for (String s : strings) {
-            System.out.println(s);
+        int a =5;
+        a<<=2;
+        System.out.println(a<<=2);
+        HashMap<String, String> map = new HashMap<>();
+        for (int i = 0; i < 13; i++) {
+            System.out.println("`````");
+            map.put("a" +i, "" + i);
         }
     }
 
-    private void lambda1() {
-        System.out.println("================");
-        List<String> strings = Arrays.asList("das", "few", "asd", "asdaw");
-        Collections.sort(strings,(Comparator<? super String>) (String a, String b)->{
-            return b.compareTo(a);
-        }
-        );
-        for (String s : strings) {
-            System.out.println(s);
-        }
-    }
 
 }
